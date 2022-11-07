d785f02c827183b12bb7dccba155aafcad6875f4
jashfeer <jashfeerabdullah@gmail.com>
2022-11-07 15:34:16 +0530

    bug fixed

--- controllers/getStatistics.go
+++ controllers/getStatistics.go
@@ -27,40 +27,56 @@ func GetStatistics(w http.ResponseWriter, r *http.Request) {
 	timeNow := time.Now().UTC()
 	fmt.Println("UTC time now: ", timeNow)
 
-	for _, value := range transactions {
+	if len(transactions) == 0 || status.Count <= 0 {
+		status.Avg = 0
+		status.Sum = 0
+		status.Count = 0
+		status.Max = 0
+		status.Min = 0
+		json.NewEncoder(w).Encode(&status)
+		return
+	}
+
+	for i, value := range transactions {
 		difference := timeNow.Sub(value.Timestamp)
 		ds := difference.Seconds()
 		if ds < 60 {
+			transactions = transactions[i:]
 			break
-		} else {
-			//Updating Sum,Count,timeMap and amountMap
-			status.Sum -= value.Amount
-			status.Count--
-			timesMap[value.Timestamp] = false
-			amountMap[value.Amount] = false
 		}
+		if status.Count <= 0 {
+			transactions = transactions[:0]
+
+		}
+		//Updating Sum,Count,timeMap and amountMap
+		status.Sum -= value.Amount
+		status.Count--
+		delete(timesMap, value.Timestamp)
+		delete(amountMap, value.Amount)
+
 	}
 
 	//updating avg
 	status.Avg = status.Sum / float64(status.Count)
 
-	min := true
-	max := true
-	for i, j := 0, len(amountInOrder)-1; i <= j; i, j = i+1, j-1 {
-		minValue_1 := timesMap[amountInOrder[i].Timestamp]
-		minValue_2 := amountMap[amountInOrder[i].Amount]
-		maxValue_1 := timesMap[amountInOrder[j].Timestamp]
-		maxValue_2 := amountMap[amountInOrder[j].Amount]
+	for i := 0; i < len(amountInOrder); i++ {
+		_, minValue_1 := timesMap[amountInOrder[i].Timestamp]
+		_, minValue_2 := amountMap[amountInOrder[i].Amount]
 
-		if minValue_1 && minValue_2 && min {
+		if minValue_1 && minValue_2 {
 			status.Min = amountInOrder[i].Amount
-			min = false
+			amountInOrder = amountInOrder[i:]
+			break
 		}
-		if maxValue_1 && maxValue_2 && max {
+	}
+
+	for j := len(amountInOrder) - 1; j >= 0; j-- {
+		_, minValue_1 := timesMap[amountInOrder[j].Timestamp]
+		_, minValue_2 := amountMap[amountInOrder[j].Amount]
+
+		if minValue_1 && minValue_2 {
 			status.Max = amountInOrder[j].Amount
-			max = false
-		}
-		if !min && !max {
+			amountInOrder = amountInOrder[:j+1]
 			break
 		}
 	}
